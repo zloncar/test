6c06acd5fbaf1d3de689a172d8c4649741d9e664
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2015-01-16 15:07:26 +0000

    Add greeter method.

--- procs.rb
+++ procs.rb
@@ -6,3 +6,9 @@ puts result.join(', ') # => 6, 9, 12, 15, 18, 21, 24
 over_4_feet = Proc.new { |i| i >= 4 }
 group= [4.1, 5.5, 3.2, 3.3, 6.1, 3.9, 4.7]
 puts group.select(&over_4_feet).join(', ') # => 4.1, 5.5, 6.1, 4.7
+
+phrase = Proc.new { 'Hello there' }
+def greeter
+  yield
+end
+puts greeter(&phrase) # => Hello there
